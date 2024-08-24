8367dceeb565a57765761f832c766b30d27b4eaa
ErMike085 <mmunozortiz706@gmail.com>
2024-08-24 01:17:45 -0500

    utilidad de las plantillas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+@section('title', 'Laravel 11')
 
-<body>
+@push('css')
+    <style>
+        body {
+            background-color: #f3f4f6;
+        }
+    </style>
+@endpush
 
-    <header></header>
-
+@section('content')
     <div class="max-w-4xl mx-auto px-4">
         {{-- <h1>Bienvenido a la página principal</h1> --}}
 
@@ -25,8 +23,4 @@
 
         <p>Hola mundo</p>
     </div>
-
-    <footer></footer>
-</body>
-
-</html>
+@endsection
