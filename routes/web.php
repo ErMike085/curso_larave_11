0f39367ac38ee9333fb288923299f05b2907f009
ErMike085 <mmunozortiz706@gmail.com>
2024-09-11 01:07:24 -0500

    Usando resource en las rutas

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,21 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', HomeController::class);
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::apiResource('posts', PostController::class);
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+// Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 
+// Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 
 // Route::get('/posts/{post}/{category?}', function ($post, $category = null) {
